0e3cab785936e07563d67a259c96e4a6ec02c076
MaxXvostik <99607335+MaxXvostik@users.noreply.github.com>
2023-01-06 14:06:05 +0300

    Update auth.go

--- src/middleware/auth.go
+++ src/middleware/auth.go
@@ -11,7 +11,7 @@ func Authentication() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		session := sessions.Default(ctx)
 		sessionId := session.Get("id")
-		if sessionId != nil {
+		if sessionId == nil {
 			ctx.JSON(http.StatusNotFound, gin.H{
 				"message": "unauthorized",
 			})
